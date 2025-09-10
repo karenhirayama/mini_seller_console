c43cb539710f512f117cec41cdd890aa1d71aaa4
karenhirayama <karenm.hirayama@gmail.com>
2025-09-10 09:30:31 -0300

    feat: Add configuration for react vite project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Index from "./pages/Index"
+import NotFound from "./pages/NotFound"
+
+import MiniSellerConsoleProvider from "./context/MiniSellerConsoleContex"
+
+const App = () => (
+  <MiniSellerConsoleProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </MiniSellerConsoleProvider>
+)
+
+export default App
